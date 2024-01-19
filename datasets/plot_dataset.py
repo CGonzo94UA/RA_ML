d896ad72ba7f499876c47920ae4d792247c1d7e0
Clara Gonzalez <cgsg4@gcloud.ua.es>
2024-01-19 16:18:08 +0100

    Plots of perceptron results

--- datasets/plot_dataset.py
+++ datasets/plot_dataset.py
@@ -1,28 +1,53 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Lee los datos desde el archivo CSV sin encabezado
-df = pd.read_csv('datasets/xor2.csv', header=None, names=['f1', 'f2', 'clase'])
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Extrae coordenadas x, y y la clase
-x = df['f1']
-y = df['f2']
-clase = df['clase']
 
-# Itera sobre las clases únicas y dibuja puntos para cada clase
-for c in clase.unique():
-    indices = clase == c
-    plt.scatter(x[indices], y[indices], label=f'Clase {c}')
+# Función para cargar el dataset generado
+def cargar_dataset():
+    df = pd.read_csv('datasets/2entradas.csv', header=None, names=['f1', 'f2', 'clase'])
+    return df
 
-# Configura las etiquetas
-plt.xlabel('Eje F1')
-plt.ylabel('Eje F2')
+# Función para visualizar el conjunto de datos y la recta del perceptrón
+def visualizar(dataset, pesos, sesgo):
+    # Extrae coordenadas x, y y la clase
+    x = dataset['f1']
+    y = dataset['f2']
+    clase = dataset['clase']
 
-# Muestra la leyenda
-# plt.legend()
+    # Itera sobre las clases únicas y dibuja puntos para cada clase
+    for c in clase.unique():
+        indices = clase == c
+        plt.scatter(x[indices], y[indices], label=f'Clase {c}')
 
-# Muestra el gráfico
-#plt.show()
+    x_range = np.linspace(min(dataset['f1']), max(dataset['f1']), 500)
+    y_range = np.linspace(min(dataset['f2']), max(dataset['f2']), 500)
 
-# Guarda el gráfico
-plt.savefig('datasets/xor2.png')
+    x1, x2 = np.meshgrid(x_range, y_range)
+    y = pesos[0] * x1 + pesos[1] * x2 + sesgo
+
+    plt.contour(x1, x2, y, levels=[0], colors='black')
+    
+    plt.xlabel('Feature1')
+    plt.ylabel('Feature2')
+    plt.legend()
+    plt.title('Conjunto de Datos y Recta del Perceptron')
+    #plt.show()
+    # Guardar la figura
+    plt.savefig('datasets/2entradas.png')
+
+
+# Cargar el dataset
+dataset = cargar_dataset()
+
+# Pesos y sesgo del perceptrón entrenado
+pesos = np.array([-45.0279, 18.7887 ])
+sesgo = 2 
+ 
+
+
+# Visualizar el conjunto de datos y la recta del perceptrón
+visualizar(dataset, pesos, sesgo)
